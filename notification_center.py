e10dc363a8177dab54c07daac0bddad4934eb075
manjiki <emouzeli@runbox.no>
2018-04-12 02:05:04 +0300

    Added support to control how much we are notified

--- notification_center.py
+++ notification_center.py
@@ -1,20 +1,27 @@
+# 
 # https://github.com/sindresorhus/weechat-notification-center
 # Requires `pip install pync`
+
+#
+# Updated by effie mouzeli:
+#
 
 import os
 import datetime
 import weechat
+import time
+from dateutil.tz import tzutc
 from pync import Notifier
-
 
 SCRIPT_NAME = 'notification_center'
 SCRIPT_AUTHOR = 'Sindre Sorhus <sindresorhus@gmail.com>'
-SCRIPT_VERSION = '1.3.0'
+SCRIPT_VERSION = '1.4.0'
 SCRIPT_LICENSE = 'MIT'
-SCRIPT_DESC = 'Pass highlights and private messages to the macOS Notification Center'
-WEECHAT_ICON = os.path.expanduser('~/.weechat/weechat.png')
+SCRIPT_DESC = 'Pass highlights and private messages to the macOS X 10.10+ Notification Center'
 
 weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, '', '')
+
+# use com.googlecode.iterm2 in activate_bundle_id for iTerm 
 
 DEFAULT_OPTIONS = {
 	'show_highlights': 'on',
@@ -24,7 +31,11 @@ DEFAULT_OPTIONS = {
 	'sound_name': 'Pong',
 	'activate_bundle_id': 'com.apple.Terminal',
 	'ignore_old_messages': 'off',
+	'reduce_notifications': 'on',
+	'notifications_interval': '300',
+	'weechat_icon': '~/.weechat/weechat.png',
 }
+
 
 for key, val in DEFAULT_OPTIONS.items():
 	if not weechat.config_is_set_plugin(key):
@@ -32,33 +43,58 @@ for key, val in DEFAULT_OPTIONS.items():
 
 weechat.hook_print('', 'irc_privmsg', '', 1, 'notify', '')
 
+# sample message from Notifier.list
+#[{'subtitle': '(null)', 'message': 'In #blabla by @somenick', 'group': 'weechat', 'delivered_at': datetime.datetime(2018, 3, 11, 15, 30, 43, tzinfo=tzutc()), 'title': 'Highlighted Message'}]
+
+def yes_notify(message_time, now_time, interval):
+    try:
+        # Find when we last had a notification
+        # last_in_queue['delivered_at'] is a datetime object
+        last_in_queue = Notifier.list(group='weechat')[-1]
+        epoch = datetime.datetime(1970, 1, 1, 0, 0, 0, tzinfo=tzutc())
+        last_message_time = int((last_in_queue['delivered_at'] - epoch).total_seconds())
+        if (now_time - last_message_time) > int(interval):
+            return True
+        else:
+            return False
+    except IndexError:
+        return True
+
+def tell_notifier(message, title, **kwargs):
+	# passing `None` or `''` still plays the default sound so we pass a lambda instead
+	sound = weechat.config_get_plugin('sound_name') if weechat.config_get_plugin('sound') == 'on' else lambda:_
+	weechat_icon = weechat.config_get_plugin('weechat_icon')
+	activate_bundle_id = weechat.config_get_plugin('activate_bundle_id')
+	Notifier.notify(message, title=title, sound=sound, appIcon=weechat_icon, activate=activate_bundle_id, group='weechat')
+
 def notify(data, buffer, date, tags, displayed, highlight, prefix, message):
 	# ignore if it's yourself
 	own_nick = weechat.buffer_get_string(buffer, 'localvar_nick')
-	if prefix == own_nick or prefix == ('@%s' % own_nick):
+	message_time = int(date)
+	now_time = int(time.time())
+	# prefix is empty on slack :/
+	if prefix in own_nick:
 		return weechat.WEECHAT_RC_OK
 
 	# ignore messages older than the configured theshold (such as ZNC logs) if enabled
 	if weechat.config_get_plugin('ignore_old_messages') == 'on':
-		message_time = datetime.datetime.utcfromtimestamp(int(date))
-		now_time = datetime.datetime.utcnow()
-
-		# ignore if the message is greater than 5 seconds old
-		if (now_time - message_time).seconds > 5:
+		# ignore if the message is greater than 5 minutes old
+		if (now_time - message_time) > 300:
+			return weechat.WEECHAT_RC_OK
+	if weechat.config_get_plugin('reduce_notifications') == 'on':
+		interval = weechat.config_get_plugin('notifications_interval')
+		if yes_notify(message_time, now_time, interval) is False:
 			return weechat.WEECHAT_RC_OK
 
-	# passing `None` or `''` still plays the default sound so we pass a lambda instead
-	sound = weechat.config_get_plugin('sound_name') if weechat.config_get_plugin('sound') == 'on' else lambda:_
-	activate_bundle_id = weechat.config_get_plugin('activate_bundle_id')
 	if weechat.config_get_plugin('show_highlights') == 'on' and int(highlight):
 		channel = weechat.buffer_get_string(buffer, 'localvar_channel')
 		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s %s' % (prefix, channel), sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
+			tell_notifier(message, title='%s %s' % (prefix, channel))
 		else:
-			Notifier.notify('In %s by %s' % (channel, prefix), title='Highlighted Message', sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
+			tell_notifier('In %s by %s' % (channel, prefix), title='Mention')
 	elif weechat.config_get_plugin('show_private_message') == 'on' and 'notify_private' in tags:
 		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s [private]' % prefix, sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
+			tell_notifier(message, title='%s [private]' % prefix)
 		else:
-			Notifier.notify('From %s' % prefix, title='Private Message', sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
+			tell_notifier('From %s' % prefix, title='Direct Message')
 	return weechat.WEECHAT_RC_OK
